4f337115e30084e0da3a84ac3a85477a985ffb8a
Michal Trela <mmichal.trela@gmail.com>
2023-02-03 18:48:43 +0100

    all algorithms implemented

--- Lab11/include/process_data.hpp
+++ Lab11/include/process_data.hpp
@@ -1,15 +1,3 @@
-#include <sys/stat.h>
-
-#include <array>
-#include <fstream>
-#include <iostream>
-#include <queue>
-#include <sstream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
 class ProcessData {
    private:
     int pid;
@@ -17,7 +5,7 @@ class ProcessData {
     int priority;
 
    public:
-    ProcessData(int priority, int burst_time, int pid) {
+    ProcessData(int burst_time, int priority, int pid) {
         ProcessData::pid = pid;
         ProcessData::burst_time = burst_time;
         ProcessData::priority = priority;
@@ -57,19 +45,3 @@ class ProcessData {
         ProcessData::burst_time = ProcessData::burst_time - 1;
     }
 };
-
-class Compare {
-   public:
-    bool operator()(ProcessData a, ProcessData b) {
-        if (a.get_burst_time() == b.get_burst_time()) {
-            return a.get_pid() > b.get_pid();
-        }
-        return a.get_burst_time() > b.get_burst_time();
-    }
-};
-
-void first_come_first_served(queue<ProcessData> &fcfs, array<ProcessData, 3> &cpu_cores, int &chronon, bool &terminate, bool end_of_file);
-
-void shortest_jobtime_first(priority_queue<ProcessData, vector<ProcessData>, Compare> &sjf, array<ProcessData, 3> &cpu_cores, int &chronon, bool &terminate, bool end_of_file);
-
-bool exists_test(const string &name);
--- Lab11/main.cpp
+++ Lab11/main.cpp
@@ -1,33 +1,49 @@
-#include "scheduler.hpp"
+#include "include/scheduler.hpp"
 
 int main(int argc, char *argv[]) {
-    int algorithm_choice;
+    int algorithm_choice = -1;
     string filename;
     int chronon = 0;
     bool end_of_file = false;
     bool terminate = false;
     array<ProcessData, 3> cpu_cores = {ProcessData(-1, -1, -1), ProcessData(-1, -1, -1), ProcessData(-1, -1, -1)};
+    array<int, 3> time_on_processor = {0, 0, 0};
 
-    queue<ProcessData> fcfs;
-    priority_queue<ProcessData, vector<ProcessData>, Compare> sjf;
+    queue<ProcessData> fcfs_rr;
+    priority_queue<ProcessData, vector<ProcessData>, CompareBurstTimeOrPID> sjf_srtf;
+    priority_queue<ProcessData, vector<ProcessData>, ComparePriorityOrPID> npq_pq_fcfs;
+    priority_queue<ProcessData, vector<ProcessData>, ComparePriorityOrBurstTime> pq_srtf;
 
-    // cout << "Choose used algorithm:" << endl;
-    // cout << "1 - FCFS, non-preemptive" << endl;
-    // cin >> algorithm_choice;
-
+    // \e[1m and \e[0m is only for command line formatting
     while (1) {
-        cout << "Enter filename:" << '\n';
+        cout << "\e[1mEnter filename:\e[0m" << '\n';
         cin >> filename;
         if (exists_test(filename)) {
             break;
         }
-        cout << "File does not exist." << '\n';
+        cout << "\e[1mFile does not exist.\e[0m" << '\n';
+    }
+
+    while (1) {
+        cout << "\e[1mChoose used algorithm:\e[0m" << '\n';
+        cout << "0 - FCFS, non-preemptive" << '\n';
+        cout << "1 - SJF, non-preemptive" << '\n';
+        cout << "2 - SRTF, preemptive" << '\n';
+        cout << "3 - RR, preemptive" << '\n';
+        cout << "4 - Priority Queue, preemptive, processes with the same priority sorted by FCFS" << '\n';
+        cout << "5 - Priority Queue, preemptive, processes with the same priority sorted by SRTF" << '\n';
+        cout << "6 - Priority Queue, non-preemptive, processes with the same priority sorted by FCFS" << '\n';
+        cin >> algorithm_choice;
+        if (algorithm_choice >= 0 && algorithm_choice <= 6) {
+            break;
+        }
+        cout << "Wrong argument." << '\n';
     }
 
     ifstream infile(filename);
     string input_line;
 
-    cout << "Chronon" << '\t' << "CPU0" << '\t' << "CPU1" << '\t' << "CPU2" << '\n';
+    cout << "\e[1mChronon" << '\t' << "\e[1mCPU0" << '\t' << "\e[1mCPU1" << '\t' << "\e[1mCPU2\e[0m" << '\n';
     while (1) {
         if (getline(infile, input_line)) {
             istringstream iss(input_line);
@@ -40,14 +56,62 @@ int main(int argc, char *argv[]) {
 
             for (int i = 1; i < buffer.size(); i++) {
                 ProcessData process_params = ProcessData(buffer[i], buffer[i++], buffer[i++]);
-                // fcfs.push(process_params);
-                sjf.push(process_params);
+                switch (algorithm_choice) {
+                    case 0:
+                        fcfs_rr.push(process_params);
+                        break;
+                    case 1:
+                        sjf_srtf.push(process_params);
+                        break;
+                    case 2:
+                        sjf_srtf.push(process_params);
+                        break;
+                    case 3:
+                        fcfs_rr.push(process_params);
+                        break;
+                    case 4:
+                        npq_pq_fcfs.push(process_params);
+                        break;
+                    case 5:
+                        pq_srtf.push(process_params);
+                        break;
+                    case 6:
+                        npq_pq_fcfs.push(process_params);
+                        break;
+                }
             }
         } else
             end_of_file = true;
-        shortest_jobtime_first(sjf, cpu_cores, chronon, terminate, end_of_file);
-        // first_come_first_served(fcfs, cpu_cores, chronon, terminate, end_of_file);
 
+        cout << chronon;
+        
+        switch (algorithm_choice) {
+            case 0:
+                first_come_first_served(fcfs_rr, cpu_cores, terminate, end_of_file);
+                break;
+            case 1:
+                shortest_remaining_time(sjf_srtf, cpu_cores, terminate, end_of_file);
+                break;
+            case 2:
+                shortest_jobtime_first(sjf_srtf, cpu_cores, terminate, end_of_file);
+                break;
+            case 3:
+                round_robin(fcfs_rr, cpu_cores, time_on_processor, terminate, end_of_file);
+                break;
+            case 4:
+                preemptive_priority_queue_with_fcfs(npq_pq_fcfs, cpu_cores, terminate, end_of_file);
+                break;
+            case 5:
+                preemptive_priority_queue_with_srtf(pq_srtf, cpu_cores, terminate, end_of_file);
+                break;
+            case 6:
+                nonpreemptive_priority_queue_with_fcfs(npq_pq_fcfs, cpu_cores, terminate, end_of_file);
+                break;
+        }
         if (terminate) break;
+
+        chronon++;
     }
+
+    cout << "\e[1mALL PROCESSES FINISHED\e[1m" << '\n';
 }
